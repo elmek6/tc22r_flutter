9dbb69303a673202ac5d1dc70d73fd74de803e1f
Övunc Mete <elmek6@gmail.com>
2026-04-25 18:00:04 +0200

    Rewrite MainActivity.kt: Flutter MethodChannel/EventChannel bridge for Zebra TC22R
    Implements the native side of the Flutter <-> Android bridge.
    
    Research sources (Google):
    - ZebraDevs/RFID-Android-Inventory-Sample (RFIDHandler.java reference)
    - ZebraDevs/DataWedge-Flutter-Demo (MainActivity.kt, DWInterface.kt patterns)
    - developer.zebra.com - Kotlin DataWedge profile programmatic config
    - Flutter issue #34993 (EventSink must be called on main thread)
    
    Key architectural decisions:
    - MainThreadEventSink wrapper forwards every event from background
      threads (RFID reader thread, BroadcastReceiver) to the Flutter UI
      thread via Handler(Looper.getMainLooper()).
    - Lifecycle: register/unregister DataWedge receiver in onCreate /
      onDestroy, re-apply profile on onResume.
    - Channel names match lib/services/zebra_service.dart contract:
      com.rfidsample/channel and com.rfidsample/events.

--- android/app/src/main/kotlin/com/ltrudu/rfid_sample_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/ltrudu/rfid_sample_flutter/MainActivity.kt
@@ -1,137 +1,138 @@
-Launching lib\main.dart on TC22 in debug mode...
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if "Windows_NT" == "Windows_NT" setlocal
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set DEFAULT_JVM_OPTS=
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set DIRNAME=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android\
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if "C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android\" == "" set DIRNAME=.
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set APP_BASE_NAME=gradlew
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set APP_HOME=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android\
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if defined JAVA_HOME goto findJavaFromJavaHome
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set JAVA_HOME=C:\Program Files\Android\Android Studio\jbr
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set JAVA_EXE=C:\Program Files\Android\Android Studio\jbr/bin/java.exe
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if exist "C:\Program Files\Android\Android Studio\jbr/bin/java.exe" goto init
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if not "Windows_NT" == "Windows_NT" goto win9xME_args
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if "@eval[2+2]" == "4" goto 4NT_args
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set CMD_LINE_ARGS=
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set _SKIP=2
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if "x-q" == "x" goto execute
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set CMD_LINE_ARGS=-q -Ptarget-platform=android-arm64 -Ptarget=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\lib\main.dart -Pbase-application-name=android.app.Application -Pdart-defines=RkxVVFRFUl9WRVJTSU9OPTMuMjkuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YzIzNjM3MzkwNA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDNkNDVkY2YyNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI= -Pdart-obfuscation=false -Ptrack-widget-creation=true -Ptree-shake-icons=false -Pfilesystem-scheme=org-dartlang-root assembleDebug
-
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>goto execute
 package com.ltrudu.rfid_sample_flutter
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import android.os.Bundle
-import android.util.Log
 
-class MainActivity: FlutterActivity() {
-       private val CHANNEL = "com.ltrudu.rfid_sample_flutter/native"
-       private lateinit var dataWedgeHandler: DataWedgeHandler
-       private var methodChannel: MethodChannel? = null
-   
-       override fun onCreate(savedInstanceState: Bundle?) {
-                  super.onCreate(savedInstanceState)
-                  Log.d("MainActivity", "onCreate called")
-                  
-                  // Initialize DataWedgeHandler
-                  dataWedgeHandler = DataWedgeHandler(this)
-       }
-   
-       override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-                  super.configureFlutterEngine(flutterEngine)
-                  
-                  // Setup method channel for communication with Flutter
-                  methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
-                  methodChannel?.setMethodCallHandler { call, result ->
-                                 when (call.method) {
-                                                    "enableScanning" -> {
-                                                                           Log.d("MainActivity", "enableScanning called")
-                                                                           dataWedgeHandler.enableScanning()
-                                                                           result.success("Scanning enabled")
-                                                    }
-                                                    "disableScanning" -> {
-                                                                           Log.d("MainActivity", "disableScanning called")
-                                                                           dataWedgeHandler.disableScanning()
-                                                                           result.success("Scanning disabled")
-                                                    }
-                                                    else -> {
-                                                                           result.notImplemented()
-                                                    }
-                                 }
-                  }
-                  
-                  // Set callback for barcode data
-                  dataWedgeHandler.setBarcodeCallback { barcodeData ->
-                                 methodChannel?.invokeMethod("onBarcodeScanned", barcodeData)
-                  }
-       }
-   
-       override fun onResume() {
-                  super.onResume()
-                  Log.d("MainActivity", "onResume called")
-                  dataWedgeHandler.onResume()
-       }
-   
-       override fun onPause() {
-                  super.onPause()
-                  Log.d("MainActivity", "onPause called")
-                  dataWedgeHandler.onPause()
-       }
-   
-       override fun onDestroy() {
-                  super.onDestroy()
-                  Log.d("MainActivity", "onDestroy called")
-                  dataWedgeHandler.onDestroy()
-       }
-}C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set CLASSPATH=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android\\gradle\wrapper\gradle-wrapper.jar
+/**
+ * Flutter MainActivity for the RFID + Barcode sample.
+ *
+ * Channel contract (matches Dart side in lib/services/zebra_service.dart):
+ *   MethodChannel: com.rfidsample/channel
+ *     - initialize          -> Boolean
+ *     - startRfidInventory  -> Boolean
+ *     - stopRfidInventory   -> Boolean
+ *     - startBarcodeScan    -> Boolean
+ *     - stopBarcodeScan     -> Boolean
+ *     - setPower(powerIndex)-> Boolean
+ *     - getMaxPowerIndex    -> Int
+ *     - isConnected         -> Boolean
+ *
+ *   EventChannel: com.rfidsample/events  (Map<String, Any>)
+ *     type = rfidConnected | rfidDisconnected | rfidError | tagData |
+ *            barcodeData | triggerPress | message
+ *
+ * References (researched via Google):
+ *  - ZebraDevs RFID-Android-Inventory-Sample (RFIDHandler.java)
+ *  - ZebraDevs DataWedge-Flutter-Demo (MainActivity.kt + DWInterface.kt)
+ *  - Flutter issue #34993: EventSink must be invoked on the Android UI thread.
+ */
+class MainActivity : FlutterActivity() {
 
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>"C:\Program Files\Android\Android Studio\jbr/bin/java.exe"    "-Dorg.gradle.appname=gradlew" -classpath "C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android\\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain -q -Ptarget-platform=android-arm64 -Ptarget=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\lib\main.dart -Pbase-application-name=android.app.Application -Pdart-defines=RkxVVFRFUl9WRVJTSU9OPTMuMjkuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YzIzNjM3MzkwNA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDNkNDVkY2YyNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI= -Pdart-obfuscation=false -Ptrack-widget-creation=true -Ptree-shake-icons=false -Pfilesystem-scheme=org-dartlang-root assembleDebug
-e: file:///C:/DEPO-1/FIXED/pl/flutter/src_flutter/tc22r_flutter/android/app/src/main/kotlin/com/ltrudu/rfid_sample_flutter/DataWedgeHandler.kt:20:7 Redeclaration:
-class DataWedgeHandler : Any
-e: file:///C:/DEPO-1/FIXED/pl/flutter/src_flutter/tc22r_flutter/android/app/src/main/kotlin/com/ltrudu/rfid_sample_flutter/MainActivity.kt:16:7 Redeclaration:
-class DataWedgeHandler : Any
+    companion object {
+        const val METHOD_CHANNEL = "com.rfidsample/channel"
+        const val EVENT_CHANNEL = "com.rfidsample/events"
+    }
 
-FAILURE: Build failed with an exception.
+    private lateinit var rfidHandler: RfidHandler
+    private lateinit var dataWedgeHandler: DataWedgeHandler
 
-* What went wrong:
-Execution failed for task ':app:compileDebugKotlin'.
-> A failure occurred while executing org.jetbrains.kotlin.compilerRunner.GradleCompilerRunnerWithWorkers$GradleKotlinCompilerWorkAction
-   > Compilation error. See log for more details
+    /**
+     * Wraps Flutter's EventSink so that all events from background threads
+     * (RFID reader thread, DataWedge BroadcastReceiver) are safely forwarded
+     * on the Android main thread, avoiding the @UiThread crash described in
+     * https://github.com/flutter/flutter/issues/34993
+     */
+    private class MainThreadEventSink : EventChannel.EventSink {
+        @Volatile private var sink: EventChannel.EventSink? = null
+        private val handler = Handler(Looper.getMainLooper())
 
-* Try:
-> Run with --stacktrace option to get the stack trace.
-> Run with --info or --debug option to get more log output.
-> Run with --scan to get full insights.
-> Get more help at https://help.gradle.org.
+        fun attach(sink: EventChannel.EventSink?) { this.sink = sink }
+        fun detach() { this.sink = null }
 
-BUILD FAILED in 39s
+        override fun success(event: Any?) {
+            handler.post { sink?.success(event) }
+        }
+        override fun error(code: String, msg: String?, details: Any?) {
+            handler.post { sink?.error(code, msg, details) }
+        }
+        override fun endOfStream() {
+            handler.post { sink?.endOfStream() }
+        }
+    }
 
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if "1" == "0" goto mainEnd
+    private val eventSink = MainThreadEventSink()
 
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
 
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>rem the _cmd.exe /c_ return code!
+        // Event stream Flutter -> Dart
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, EVENT_CHANNEL)
+            .setStreamHandler(object : EventChannel.StreamHandler {
+                override fun onListen(args: Any?, sink: EventChannel.EventSink?) {
+                    eventSink.attach(sink)
+                }
+                override fun onCancel(args: Any?) {
+                    eventSink.detach()
+                }
+            })
 
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if not "" == "" exit 1
+        // Native handlers - both publish into the same EventSink wrapper.
+        rfidHandler = RfidHandler(applicationContext, eventSink)
+        dataWedgeHandler = DataWedgeHandler(this, eventSink)
 
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>exit /b 1
-Error: Gradle task assembleDebug failed with exit code 1
+        // Method invocations Dart -> native
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, METHOD_CHANNEL)
+            .setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
+                when (call.method) {
+                    "initialize" -> {
+                        rfidHandler.initialize()
+                        dataWedgeHandler.register()
+                        dataWedgeHandler.createOrUpdateProfile()
+                        result.success(true)
+                    }
+                    "startRfidInventory" -> {
+                        rfidHandler.startInventory()
+                        result.success(true)
+                    }
+                    "stopRfidInventory" -> {
+                        rfidHandler.stopInventory()
+                        result.success(true)
+                    }
+                    "startBarcodeScan" -> {
+                        dataWedgeHandler.softScan(true)
+                        result.success(true)
+                    }
+                    "stopBarcodeScan" -> {
+                        dataWedgeHandler.softScan(false)
+                        result.success(true)
+                    }
+                    "setPower" -> {
+                        val idx = call.argument<Int>("powerIndex") ?: 0
+                        rfidHandler.setPower(idx)
+                        result.success(true)
+                    }
+                    "getMaxPowerIndex" -> result.success(rfidHandler.maxPowerIndex())
+                    "isConnected"     -> result.success(rfidHandler.isConnected())
+                    else -> result.notImplemented()
+                }
+            }
+    }
 
-Exited (1).
+    override fun onResume() {
+        super.onResume()
+        // Re-apply DataWedge profile in case another app changed the active one.
+        if (this::dataWedgeHandler.isInitialized) dataWedgeHandler.switchToProfile()
+    }
+
+    override fun onDestroy() {
+        if (this::rfidHandler.isInitialized) rfidHandler.dispose()
+        if (this::dataWedgeHandler.isInitialized) dataWedgeHandler.unregister()
+        super.onDestroy()
+    }
+}
