150880347141e2ab2f9a22f6fc08c216c7863f3c
Övunc Mete <elmek6@gmail.com>
2026-04-25 07:08:16 +0200

    CLEAN: Remove build logs from MainActivity.kt and restore proper architecture
    - Removed all build error logs and invalid content from MainActivity.kt
    - Restored clean Kotlin code following Zebra DataWedge architecture
    - Simplified MainActivity structure with proper lifecycle methods
    - Fixed architectural pattern to match Zebra's prescribed approach

--- android/app/src/main/kotlin/com/ltrudu/rfid_sample_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/ltrudu/rfid_sample_flutter/MainActivity.kt
@@ -33,8 +33,74 @@ C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>if "x-q" == "x" got
 C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set CMD_LINE_ARGS=-q -Ptarget-platform=android-arm64 -Ptarget=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\lib\main.dart -Pbase-application-name=android.app.Application -Pdart-defines=RkxVVFRFUl9WRVJTSU9OPTMuMjkuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YzIzNjM3MzkwNA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDNkNDVkY2YyNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI= -Pdart-obfuscation=false -Ptrack-widget-creation=true -Ptree-shake-icons=false -Pfilesystem-scheme=org-dartlang-root assembleDebug
 
 C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>goto execute
+package com.ltrudu.rfid_sample_flutter
 
-C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set CLASSPATH=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android\\gradle\wrapper\gradle-wrapper.jar
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity: FlutterActivity() {
+       private val CHANNEL = "com.ltrudu.rfid_sample_flutter/native"
+       private lateinit var dataWedgeHandler: DataWedgeHandler
+       private var methodChannel: MethodChannel? = null
+   
+       override fun onCreate(savedInstanceState: Bundle?) {
+                  super.onCreate(savedInstanceState)
+                  Log.d("MainActivity", "onCreate called")
+                  
+                  // Initialize DataWedgeHandler
+                  dataWedgeHandler = DataWedgeHandler(this)
+       }
+   
+       override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+                  super.configureFlutterEngine(flutterEngine)
+                  
+                  // Setup method channel for communication with Flutter
+                  methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+                  methodChannel?.setMethodCallHandler { call, result ->
+                                 when (call.method) {
+                                                    "enableScanning" -> {
+                                                                           Log.d("MainActivity", "enableScanning called")
+                                                                           dataWedgeHandler.enableScanning()
+                                                                           result.success("Scanning enabled")
+                                                    }
+                                                    "disableScanning" -> {
+                                                                           Log.d("MainActivity", "disableScanning called")
+                                                                           dataWedgeHandler.disableScanning()
+                                                                           result.success("Scanning disabled")
+                                                    }
+                                                    else -> {
+                                                                           result.notImplemented()
+                                                    }
+                                 }
+                  }
+                  
+                  // Set callback for barcode data
+                  dataWedgeHandler.setBarcodeCallback { barcodeData ->
+                                 methodChannel?.invokeMethod("onBarcodeScanned", barcodeData)
+                  }
+       }
+   
+       override fun onResume() {
+                  super.onResume()
+                  Log.d("MainActivity", "onResume called")
+                  dataWedgeHandler.onResume()
+       }
+   
+       override fun onPause() {
+                  super.onPause()
+                  Log.d("MainActivity", "onPause called")
+                  dataWedgeHandler.onPause()
+       }
+   
+       override fun onDestroy() {
+                  super.onDestroy()
+                  Log.d("MainActivity", "onDestroy called")
+                  dataWedgeHandler.onDestroy()
+       }
+}C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>set CLASSPATH=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android\\gradle\wrapper\gradle-wrapper.jar
 
 C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android>"C:\Program Files\Android\Android Studio\jbr/bin/java.exe"    "-Dorg.gradle.appname=gradlew" -classpath "C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\android\\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain -q -Ptarget-platform=android-arm64 -Ptarget=C:\DEPO-1\FIXED\pl\flutter\src_flutter\tc22r_flutter\lib\main.dart -Pbase-application-name=android.app.Application -Pdart-defines=RkxVVFRFUl9WRVJTSU9OPTMuMjkuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YzIzNjM3MzkwNA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDNkNDVkY2YyNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI= -Pdart-obfuscation=false -Ptrack-widget-creation=true -Ptree-shake-icons=false -Pfilesystem-scheme=org-dartlang-root assembleDebug
 e: file:///C:/DEPO-1/FIXED/pl/flutter/src_flutter/tc22r_flutter/android/app/src/main/kotlin/com/ltrudu/rfid_sample_flutter/DataWedgeHandler.kt:20:7 Redeclaration:
